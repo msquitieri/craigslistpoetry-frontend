009e2ea01f1932750943f1d8c01a61c89b7ce76a
Michael Squitieri <michael.squitieri@gmail.com>
2016-05-08 01:49:48 -0400

    hook up load more poems button

--- app/controllers/poems.js
+++ app/controllers/poems.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  page: 1,
 
   sortedPoems: Ember.computed('model.@each.id', function() {
     if (this.get('model')) {
@@ -11,6 +12,13 @@ export default Ember.Controller.extend({
   actions: {
     displayPoem(poem) {
       console.log(poem.id);
+    },
+
+    loadMorePoems() {
+      let nextPage = this.get('page') + 1;
+      this.set('page', nextPage);
+
+      this.store.query('poem', { page: nextPage });
     }
   }
 });
