1b453dcc74bd1376a6ca015814db702498417d53
Michael Squitieri <michael.squitieri@gmail.com>
2016-05-09 00:34:34 -0400

    add ember-scroll-to addon and hook it up to header links

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  scroller: Ember.inject.service(),
+
   actions: {
     createNewPoem() {
       return this.store.createRecord('poem').save();
+    },
+
+    scrollTo(selector) {
+      let $element = Ember.$(selector);
+      let options = {
+        duration: 500,
+        offset: -70
+      };
+
+      this.get('scroller').scrollVertical($element, options);
     }
   }
 });
