f54fcf6a4529911734f419a391b86639214d408d
Michael Squitieri <michael.squitieri@gmail.com>
2016-05-17 23:26:50 -0400

    hook up facebook share

--- app/components/poem-modal.js
+++ app/components/poem-modal.js
@@ -21,5 +21,23 @@ export default Ember.Component.extend({
 
   didRender() {
     this.$('#poem-modal').modal('show');
+  },
+
+  actions: {
+    facebookShare() {
+      let poemId = this.get('poem.id');
+      let url = `${ENV.defaultHost}/poems/${poemId}`;
+      let text = `${this.get('poem.previewText')}...`;
+      let header = `Craigslist Poetry | Poem ${poemId}`;
+      let image = `${ENV.defaultHost}/images/logo_small.png`;
+
+      FB.ui({
+        method: 'feed',
+        link: url,
+        picture: image,
+        name: header,
+        description: text
+      });
+    }
   }
 });
