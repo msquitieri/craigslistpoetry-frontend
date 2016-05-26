bd02f6b5a92cc91ba5036021961a26488831838d
Michael Squitieri <michael.squitieri@gmail.com>
2016-05-26 00:23:59 -0400

    be able to use the left and right keys to go to new poems while in the modal

--- app/components/poem-modal.js
+++ app/components/poem-modal.js
@@ -6,13 +6,38 @@ export default Ember.Component.extend({
     this.$('#poem-modal').on('hidden.bs.modal', () => {
       this.sendAction('onModalClose');
     });
+
+    this.$(document).on('keydown', (e) => {
+      // Left Key
+      if (e.which === 37) {
+        this.send('goToPreviousPoem');
+      } else if (e.which === 39) { // Right Key
+        this.send('goToNextPoem');
+      }
+    });
   },
 
   willDestroyElement() {
     this.$('#poem-modal').off('hidden.bs.modal');
+
+    this.$(document).off('keydown');
   },
 
   didRender() {
     this.$('#poem-modal').modal('show');
+  },
+
+  actions: {
+    goToPreviousPoem() {
+      let newId = parseInt(this.get('poem.id')) + 1;
+
+      this.sendAction('moveToNewPoem', newId);
+    },
+
+    goToNextPoem() {
+      let newId = parseInt(this.get('poem.id')) - 1;
+
+      this.sendAction('moveToNewPoem', newId);
+    }
   }
 });
--- app/controllers/poem.js
+++ app/controllers/poem.js
@@ -4,6 +4,12 @@ export default Ember.Controller.extend({
   actions: {
     transitionToPoems() {
       this.transitionToRoute('poems');
+    },
+
+    moveToNewPoem(id) {
+      if (this.store.peekRecord('poem', id)) {
+        this.transitionToRoute('poem', id);
+      }
     }
   }
 });
