295bb44911aa9f9e385e84734de5407db72591f5
Michael Squitieri <michael.squitieri@gmail.com>
2016-10-28 23:12:26 -0400

    add ability to send notifications

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,16 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Route.extend({
+  notifications: Ember.inject.service('notification-messages'),
 
   actions: {
+    sendNotification(message) {
+      this.get('notifications').success(message, {
+        autoClear: true,
+        clearDuration: 2000
+      });
+    },
+
     displayPoem(poem) {
       this.transitionTo('poem', poem);
     }
